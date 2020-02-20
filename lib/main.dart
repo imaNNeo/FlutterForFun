e4b2cb483ffefbb6e7920b85104be7676bb2f4f4
imaNNeoFighT <iman.neofight@gmail.com>
2020-02-20 14:31:26 +0330

    added backup comments

--- lib/main.dart
+++ lib/main.dart
@@ -12,3 +12,55 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+
+//textTheme: GoogleFonts.montserratTextTheme(),
+//primaryColor: Color(0xFF607D8B),
+//primaryColorDark: Color(0xFF455A64),
+//accentColor: Color(0xFF9E9E9E),
+
+
+//List<User> users = [
+//  User(20, 'kevinclark', 'https://avatars3.githubusercontent.com/u/20?v=4'),
+//  User(21, 'technoweenie', 'https://avatars3.githubusercontent.com/u/21?v=4'),
+//  User(22, 'macournoyer', 'https://avatars3.githubusercontent.com/u/22?v=4'),
+//  User(20, 'kevinclark', 'https://avatars3.githubusercontent.com/u/23?v=4'),
+//];
+
+// test colors
+//List<Color> colors = [
+//  Colors.purpleAccent,
+//  Colors.green,
+//  Colors.blueAccent,
+//  Colors.yellow,
+//  Colors.deepPurple,
+//  Colors.blueGrey,
+//  Colors.pink,
+//  Colors.teal
+//];
+
+//class User {
+//  final int id;
+//  final String loginName;
+//  final String avatarUrl;
+//
+//  User(this.id, this.loginName, this.avatarUrl);
+//
+//  static User parseUser(Map<String, dynamic> json) {
+//    return User(
+//      json['id'],
+//      json['login'],
+//      json['avatar_url'],
+//    );
+//  }
+//
+//  static List<User> parseUsers(List<dynamic> usersJson) {
+//    return usersJson.map((json) => User.parseUser(json)).toList();
+//  }
+//}
+
+
+//extension on int {
+//  double toRadians() => (3.14 / 180.0) * this;
+//}
